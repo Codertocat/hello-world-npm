dead32b00728cd8aa4a90e5f4c2c47784d913619
2115-27 <103985748+2115-27@users.noreply.github.com>
2022-11-09 11:35:58 +0530

    Create sample3.ts

---
+++ src/app/apps/sample3.ts
@@ -0,0 +1,10 @@
+/**
+ * Web Animations `@angular/platform-browser/animations`
+ * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
+ * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
+ **/
+
+import 'zone.js';  // Included with Angular CLI.
+
+
+
